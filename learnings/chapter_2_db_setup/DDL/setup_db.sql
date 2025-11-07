b16abddeaa8a73ffb8667c6b30031f6752b610ec
Rutvik J Shah <38734146+rjs-7399@users.noreply.github.com>
2025-11-07 22:12:02 +0530

    SFCB-007: Created separate folders for DDL and DML (#8)
    * SFCB-007: Created separate folders for DDL and DML
    
    * SFCB-007: Created separate folders for DDL and DML
    
    * SFCB-007: Created separate folders for DDL and DML

---
+++ learnings/chapter_2_db_setup/DDL/setup_db.sql
@@ -0,0 +1,5 @@
+-- Drop the mydatabase DB if it already exists
+DROP DATABASE IF EXISTS mydatabase;
+
+-- Create mydatabase DB
+CREATE DATABASE mydatabase;
--- learnings/chapter_2_db_setup/setup_db.sql
+++
@@ -1,2 +0,0 @@
-DROP DATABASE IF EXISTS mydatabase;
-CREATE DATABASE mydatabase;
