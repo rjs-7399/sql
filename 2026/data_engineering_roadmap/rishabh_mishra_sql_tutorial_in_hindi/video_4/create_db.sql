94b73c4c75b67990eb01b4c592f5f60cdddc0e8a
Rutvik J Shah <38734146+rjs-7399@users.noreply.github.com>
2026-03-04 12:11:50 +0530

    RISHABH-SHARMA-SQL-VIDEO-4: Create DB instructions (#36)
    * RISHABH-SHARMA-SQL-VIDEO-4: Create DB instructions
    
    * RISHABH-SHARMA-SQL-VIDEO-4: Create DB instructions
    
    * RISHABH-SHARMA-SQL-VIDEO-4: Create Table and their instructions

---
+++ 2026/data_engineering_roadmap/rishabh_mishra_sql_tutorial_in_hindi/video_4/create_db.sql
@@ -0,0 +1,6 @@
+--- This will create the database in the sql server
+CREATE DATABASE testdb;
+
+--- This will simply create the database in the sql server,
+--- if we want to modify the special instructions like permissions
+--- then we can change them in the pgadmin settings.
