4fc9c83a1f017954876cf45a7b26541cf2397b72
rutvikshah <rutvikjshah@gmail.com>
2026-03-13 19:03:17 +0530

    RISHABH-SHARMA-SQL-VIDEO-6: Select all the specific columns from the given table

---
+++ 2026/data_engineering_roadmap/rishabh_mishra_sql_tutorial_in_hindi/video_6/select_specific_cols.sql
@@ -0,0 +1,5 @@
+-- Select specific fields from the given tables
+-- Here we specify the fields by comma
+-- And this is the standard practice to select the fields with comma instead of selecting them with '*'.
+-- Because select all the fields using * will degrade the performance.
+SELECT id, first_name FROM customers;
